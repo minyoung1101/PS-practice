d9b31c71412a5fc09975de0c720c8da71be754b7
minyoung1101 <my11612@naver.com>
2025-06-24 18:33:14 +0900

    Solve 예산

---
+++ programmers/level1/예산.js
@@ -0,0 +1,14 @@
+// 숫자 짝꿍 :12982
+// https://school.programmers.co.kr/learn/courses/30/lessons/12982
+
+function solution(d, budget) {
+    let used_b = 0;
+    let result = 0;
+    for(let b of d.sort((a,b)=>a-b)) {
+        if(used_b+b <= budget) {
+            used_b += b;
+            result++;
+        }
+    }
+    return result;
+}
