a429246c0afc19e1e1fbb9a02a671a307b9c2f40
minyoung1101 <my11612@naver.com>
2025-07-29 15:12:38 +0900

    Solved: 특이한 정렬

---
+++ programmers/level0/특이한 정렬.js
@@ -0,0 +1,6 @@
+// 특이한 정렬
+// https://school.programmers.co.kr/learn/courses/30/lessons/120880
+
+function solution(numlist, n) {
+  return numlist.sort((a, b) => Math.abs(n - a) - Math.abs(n - b) || b - a);
+}
